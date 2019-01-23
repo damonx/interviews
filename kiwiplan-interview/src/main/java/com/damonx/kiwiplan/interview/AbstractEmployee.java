823caca8f1cf66ac3be852e99f0c5cea9680536f
Damon Xu <akldamonx@icloud.com>
2019-01-23 19:41:09 +1300

    Removed copyright info

--- kiwiplan-interview/src/main/java/com/damonx/kiwiplan/interview/AbstractEmployee.java
+++ kiwiplan-interview/src/main/java/com/damonx/kiwiplan/interview/AbstractEmployee.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) Fisher and Paykel Appliances.
- *
- * This document is copyright. Except for the purpose of fair reviewing, no part
- * of this publication may be reproduced or transmitted in any form or by any
- * means, electronic or mechanical, including photocopying, recording, or any
- * information storage and retrieval system, without permission in writing from
- * the publisher. Infringers of copyright render themselves liable for
- * prosecution.
- */
 package com.damonx.kiwiplan.interview;
 
 import java.util.Objects;
